642707940eee159644e247113918e3210b156d1d
Ambupada Muduli <91827366+Ambupada-Muduli@users.noreply.github.com>
2023-07-24 21:25:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import "./styles.css";
+import React, { useState, useEffect, useRef } from "react";
+
+const FAILURE_COUNT = 10;
+const LATENCY = 200;
+
+function getRandomBool(n) {
+  const threshold = 1000;
+  if (n > threshold) n = threshold;
+  return Math.floor(Math.random() * threshold) % n === 0;
+}
+
+function getSuggestions(text) {
+  var pre = "pre";
+  var post = "post";
+  var results = [];
+  if (getRandomBool(2)) {
+    results.push(pre + text);
+  }
+  if (getRandomBool(2)) {
+    results.push(text);
+  }
+  if (getRandomBool(2)) {
+    results.push(text + post);
+  }
+  if (getRandomBool(2)) {
+    results.push(pre + text + post);
+  }
+  return new Promise((resolve, reject) => {
+    const randomTimeout = Math.random() * LATENCY;
+    setTimeout(() => {
+      if (getRandomBool(FAILURE_COUNT)) {
+        reject();
+      } else {
+        resolve(results);
+      }
+    }, randomTimeout);
+  });
+}
+
+export default function App() {
+  const inputRef = useRef();
+  const suggestionAreaRef = useRef();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [list, updateList] = useState([]);
+  const [suggestionAreaVisible, setSuggestionAreaVisiblity] = useState(false);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSearchQuery(value);
+    makeApiCall(value);
+  };
+
+  const makeApiCall = async (query) => {
+    try {
+      let resp = await getSuggestions(query);
+      updateList(resp);
+    } catch (e) {
+      updateList([]);
+      console.error("Error while getting suggestons list ", e);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", (e) => {
+      if (
+        e.target !== inputRef.current &&
+        e.target !== suggestionAreaRef.current
+      ) {
+        setSuggestionAreaVisiblity(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("click", () => {});
+    };
+  });
+
+  return (
+    <main className="App">
+      <input
+        type="text"
+        name="search"
+        placeholder="search"
+        id="search"
+        onFocus={() => setSuggestionAreaVisiblity(true)}
+        onChange={handleChange}
+        value={searchQuery}
+        ref={inputRef}
+      />
+      {suggestionAreaVisible && (
+        <div id="suggestion-area" ref={suggestionAreaRef}>
+          {list.map((e) => (
+            <div onClick={() => setSearchQuery(e)}>{e}</div>
+          ))}
+        </div>
+      )}
+    </main>
+  );
+}
